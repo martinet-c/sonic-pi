fa5fbd4d4c14af4e6380f0143b064d0c24e9bd45
MARTINET-C <charlie.martinet@harmonie-mutuelle.fr>
2023-02-07 17:06:35 +0100

    120bpm

--- 80s.rb
+++ 80s.rb
@@ -1,37 +1,37 @@
-use_bpm 60
+use_bpm 120
 
 in_thread do
-  sleep 0.25
+  sleep 0.5
   loop do
     sample :drum_heavy_kick
-    sleep 0.5
+    sleep 1
   end
 end
 
 in_thread do
   use_synth :blade
   loop do
-    play :B3, release: 0.25
-    sleep 0.125
-    play :B3, release: 0.25
+    play :B3, release: 0.5
     sleep 0.25
+    play :B3, release: 0.5
+    sleep 0.5
     
-    play :B3, release: 0.25
-    sleep 0.125
-    play :B3, release: 0.25
+    play :B3, release: 0.5
     sleep 0.25
+    play :B3, release: 0.5
+    sleep 0.5
     
-    play :B3, release: 0.25
-    sleep 0.25
-    play :B1, release: 0.25
-    sleep 0.25
+    play :B3, release: 0.5
+    sleep 0.5
+    play :B1, release: 0.5
+    sleep 0.5
     
-    play :B4, release: 0.25
-    sleep 0.125
-    play :B4, release: 0.25
-    sleep 0.375
-    
+    play :B4, release: 0.5
     sleep 0.25
+    play :B4, release: 0.5
+    sleep 0.75
+    
+    sleep 0.5
   end
 end
 
@@ -39,31 +39,31 @@ use_synth :prophet
 loop do
   
   2.times do |count|
-    play :D4
+    play :D4, release: 2
+    sleep 0.5
+    play :D4, release: 2
+    sleep 0.5
+    play :B3, release: 2
+    sleep 0.5
+    play :Db4, release: 1
     sleep 0.25
-    play :D4
-    sleep 0.25
-    play :B3
-    sleep 0.25
-    play :Db4, release: 0.5
-    sleep 0.125
-    play :D4
-    sleep 0.25
-    play :E4
+    play :D4, release: 2
+    sleep 0.5
+    play :E4, release: 2
     if count % 2 == 0
-      sleep 0.875
+      sleep 1.750
     else
-      sleep 0.375
+      sleep 0.750
     end
   end
   
-  play :Gb4
-  sleep 0.25
-  play :D4
+  play :Gb4, release: 2
   sleep 0.5
-  play :Db4
-  sleep 0.25
-  play :B3, release: 2
-  sleep 3.5
+  play :D4, release: 2
+  sleep 1
+  play :Db4, release: 2
+  sleep 0.5
+  play :B3, release: 4
+  sleep 7
   
 end
