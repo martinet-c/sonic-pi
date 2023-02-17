198684c314091635c7ff013d8aa3e0e643d47b97
MARTINET-C <charlie.martinet@harmonie-mutuelle.fr>
2023-02-17 10:04:54 +0100

    keys --> notes

--- waiting-room.rb
+++ waiting-room.rb
@@ -25,45 +25,45 @@ in_thread do
 end
 
 in_thread do
-  keys = [[:B4, :G5, :D5,  :B4, :C5], [:B4, :G5,  :D5, :B4, :C5], [:B4, :G5, :D5, :B4, :C5], [:B4, :G5, :D5, :B4, :C5],
+  notes = [[:B4, :G5, :D5,  :B4, :C5], [:B4, :G5,  :D5, :B4, :C5], [:B4, :G5, :D5, :B4, :C5], [:B4, :G5, :D5, :B4, :C5],
           [:C5, :A5, :Gb5, :C5, :D5], [:A4, :Gb5, :A4, :B4, :C5], [:B4, :G5, :D5, :B4, :C5], [:B4, :G5, :D5, :B4, :C5]]
   use_synth :kalimba
   i = 0
   sleep 1
   loop do
-    ikeys = keys[i]
-    play ikeys[0], amp: 2
+    inotes = notes[i]
+    play inotes[0], amp: 2
     sleep 1
-    play ikeys[0], amp: 2
+    play inotes[0], amp: 2
     sleep 1
-    play ikeys[0], amp: 2
+    play inotes[0], amp: 2
     sleep 0.25
-    play ikeys[1], amp: 2
+    play inotes[1], amp: 2
     sleep 0.5
-    play ikeys[2], amp: 2
+    play inotes[2], amp: 2
     sleep 0.25
-    play ikeys[3], amp: 2
+    play inotes[3], amp: 2
     sleep 0.5
-    play ikeys[4], amp: 2
+    play inotes[4], amp: 2
     sleep 0.5
     i = (i<7 ? i+1 : 0)
   end
 end
 
-keys = [[:B4, :C5, :D5, :C5, :B4], [:B4, :C5, :D5, :E5, :B4], [:B4, :C5, :D5, :C5, :D5], [:B4, :C5, :A4, :G4, :G4]]
+notes = [[:B4, :C5, :D5, :C5, :B4], [:B4, :C5, :D5, :E5, :B4], [:B4, :C5, :D5, :C5, :D5], [:B4, :C5, :A4, :G4, :G4]]
 use_synth :hollow
 i = 0
 loop do
-  ikeys = keys[i]
-  play ikeys[0], release: 2
+  inotes = notes[i]
+  play inotes[0], release: 2
   sleep 1
-  play ikeys[1], release: 4
+  play inotes[1], release: 4
   sleep 4
-  play ikeys[2], release: 3
+  play inotes[2], release: 3
   sleep 3
-  play ikeys[3], release: 2
+  play inotes[3], release: 2
   sleep 1
-  play ikeys[4], release: 2
+  play inotes[4], release: 2
   sleep 7
   i = (i<3 ? i+1 : 0)
 end
